58016f05513be15ddca44e602bc2cc606e563d2e
wdyjason <ddd123321456555@qq.com>
2020-07-23 10:18:41 +0800

    [wdyjason] feat: finish exercise-01

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -8,5 +8,10 @@ export default function formatNumber(number, option) {
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  const floatNum = parseFloat(number).toFixed(2);
+  const returnValue = floatNum.toString();
+  if (option) {
+    return `$ ${returnValue}`;
+  }
+  return returnValue;
 }
