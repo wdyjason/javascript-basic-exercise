b1466eb3ccd099dcd199e53b27ef5eaa6526d1b9
wdyjason <ddd123321456555@qq.com>
2020-07-23 13:27:14 +0800

    [wdyjason] feat: finish exercise-05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -1,3 +1,5 @@
+import { parse } from "@babel/core";
+
 export default function flattenArray(array) {
   // This function flattens a nested array into a sequence.
   //
@@ -5,6 +7,17 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) throw new Error('Flatten undefined or null array');
+  if (array.length === 0) return [];
+  const reslut = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j += 1) {
+        reslut.push(array[i][j]);
+      }
+    } else {
+      reslut.push(array[i]);
+    }
+  }
+  return reslut;
 }
