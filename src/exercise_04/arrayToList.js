c092216aa66987c91f72cf6c6e8bf5e36d2455c5
wdyjason <ddd123321456555@qq.com>
2020-07-23 12:58:31 +0800

    [wdyjason] feat: finish exercise-04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,30 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (array === undefined || array === null) throw new Error('Creating list from undefined array');
+  if (array.length === 0) throw new Error('Creating list from empty array');
+  class LinkedNode {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
 
-  throw new Error('Please delete this line and implement the function');
+    getLastNode() {
+      let lastNode = this;
+      while (lastNode.next != null) {
+        lastNode = lastNode.next;
+      }
+      return lastNode;
+    }
+
+    addNextNode(value) {
+      if (this.value != null) this.getLastNode().next = new LinkedNode(value);
+      else this.value = value;
+    }
+  }
+  const resList = new LinkedNode(null);
+  array.forEach((element) => {
+    resList.addNextNode(element);
+  });
+  return resList;
 }
